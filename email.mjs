400526f51239ff1da042ea0d441bce3e4d5b1b6f
Busola Tom <busolatom@gmail.com>
2024-06-20 15:25:36 +0000

    Sending email in node.js with nodemailer- no mailTransport export

---
+++ email.mjs
@@ -0,0 +1,44 @@
+import {program} from 'commander';
+import {createTransport} from 'nodemailer';
+
+const mailTransport = createTransport({
+    host: 'smtppro.zoho.com'  ,
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'admin@shopa.life',
+        pass: 'E2uJpsHeHZj4',
+    }
+});
+
+program 
+.option('-s, --subject')
+.option('-t, --to')
+.option('-m, --message');
+
+program.parse();
+
+const options = program.opts();
+const args = program.args;
+console.log(options);
+console.log(args);
+if (options.subject && options.to && options.message) {
+    // send email
+    mailTransport.sendMail({
+        from: 'noreply@shopa.life',
+        to: args[1],
+        subject: args[0],
+        text: args[2]
+    })
+    .then(console.log)
+    .catch(console.log);
+//     console.log ('Message sent');
+} else if (options.subject && options.to) {
+    console.log ('Please enter your message');
+} else if (options.to && options.message) {
+    console.log ('Enter your subject');
+} else if (options.subject && options.message) {
+    console.log ('Please enter your recipient');
+} else {
+    console.log ('Some details are missing');
+}
